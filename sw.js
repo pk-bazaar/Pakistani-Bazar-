99262c50c30f4d1598a5cdefe254c5fa36c38f59
zaidAwan-crypto <zaidsahib302@gmail.com>
2026-08-06 00:06:50 +0500

    sw.js

--- sw.js
+++ sw.js
@@ -6,4 +6,8 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(clients.claim());
 });
 
-self.addEventListener('fetch', (event) => {});
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
