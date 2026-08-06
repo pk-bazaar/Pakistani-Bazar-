13a151f46fef67e16598a75e1f867b0c6cce7fcc
zaidAwan-crypto <zaidsahib302@gmail.com>
2026-08-06 10:04:15 +0500

    sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,26 @@
+const CACHE_NAME = 'pakistani-bazaar-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json'
+];
+
+// Install Service Worker
 self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
   self.skipWaiting();
 });
 
+// Activate Service Worker
 self.addEventListener('activate', (event) => {
   event.waitUntil(clients.claim());
 });
 
+// Fetch Events
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     fetch(event.request).catch(() => caches.match(event.request))
